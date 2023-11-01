8592db84610af3eba0e4dec12a9c9be2b2a79746
ujjawalPeak01 <119571874+ujjawalPeak01@users.noreply.github.com>
2023-11-01 21:44:16 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import json
+import numpy as np
+import torch
+from transformers import pipeline
+import base64
+from io import BytesIO
+
+
+class InferlessPythonModel:
+  def initialize(self):
+      self.generator = pipeline("text-generation", model="codellama/CodeLlama-34b-Python-hf")
+
+  def infer(self, inputs):
+    prompt = inputs["prompt"]
+    pipeline_output = self.generator(prompt)
+    return {"generated_result": pipeline_output}
+
+  def finalize(self,args):
+    self.generator = None
