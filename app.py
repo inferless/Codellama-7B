1830fb6780ac48908270999a84689cd195e5f11e
rbgo404 <150957746+rbgo404@users.noreply.github.com>
2023-11-23 10:30:11 +0000

    package

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from transformers import AutoTokenizer
 from auto_gptq import AutoGPTQForCausalLM
 import base64
 from io import BytesIO
+import pkg_resources
 
 
 class InferlessPythonModel:
@@ -23,7 +24,22 @@ class InferlessPythonModel:
     input_ids = self.tokenizer(prompt, return_tensors='pt').input_ids.cuda()
     output = self.model.generate(inputs=input_ids, temperature=0.7, max_new_tokens=512)
     result = self.tokenizer.decode(output[0])
-    return {"generated_result": result}
+    package_name = "auto-gptq"
+    version = self.get_package_version(package_name)
+    return {"generated_result": f'version {version}'}
+
+  def get_package_version(self,package_name):
+    try:
+        # Use pkg_resources to get the distribution
+        distribution = pkg_resources.get_distribution(package_name)
+        
+        # Return the version of the package
+        return distribution.version
+    except pkg_resources.DistributionNotFound:
+        # Handle the case where the package is not installed
+        return None
+
+# Example usage for "auto-gptq"
 
   def finalize(self,args):
     self.tokenizer = None
