b97328be4da1a56ab358f2a0652d9706b1da1362
rbgo404 <150957746+rbgo404@users.noreply.github.com>
2023-11-24 08:57:39 +0000

    for gptq

--- app.py
+++ app.py
@@ -1,46 +1,36 @@
-import json
-import numpy as np
-import torch
-from transformers import AutoTokenizer
-from auto_gptq import AutoGPTQForCausalLM
-import base64
-from io import BytesIO
-import pkg_resources
+import os
+from vllm import SamplingParams
+from vllm import LLM
+from huggingface_hub import snapshot_download
 
 
 class InferlessPythonModel:
-  def initialize(self):
-      self.tokenizer = AutoTokenizer.from_pretrained("TheBloke/CodeLlama-34B-Python-GPTQ", use_fast=True)
-      self.model = AutoGPTQForCausalLM.from_quantized(
-        "TheBloke/CodeLlama-34B-Python-GPTQ",
-        use_safetensors=True,
-        device="cuda:0",
-        quantize_config=None,
-        inject_fused_attention=False
-      )
+    def initialize(self):
+        self.template = """SYSTEM: You are a helpful assistant.
+        USER: {}
+        ASSISTANT: """
+        snapshot_download(
+            "TheBloke/CodeLlama-34B-Python-GPTQ",
+            local_dir="/model",
+            token="<<your_token>>",
+        )
+        self.llm = LLM(
+          model="/model",
+          quantization="gptq")
+    
+    def infer(self, inputs):
+        print("inputs[prompt] -->", inputs["prompt"], flush=True)
+        prompts = inputs["prompt"]
+        print("Prompts -->", prompts, flush=True)
+        sampling_params = SamplingParams(
+            temperature=1.0,
+            top_p=1,
+            max_tokens=512
+        )
+        result = self.llm.generate(prompts, sampling_params)
+        result_output = [output.outputs[0].text for output in result]
 
-  def infer(self, inputs):
-    prompt = inputs["prompt"]
-    input_ids = self.tokenizer(prompt, return_tensors='pt').input_ids.cuda()
-    output = self.model.generate(inputs=input_ids, temperature=0.7, max_new_tokens=512)
-    result = self.tokenizer.decode(output[0])
-    package_name = "auto-gptq"
-    version = self.get_package_version(package_name)
-    return {"generated_result": f'version {version}'}
+        return {"result": result_output[0]}
 
-  def get_package_version(self,package_name):
-    try:
-        # Use pkg_resources to get the distribution
-        distribution = pkg_resources.get_distribution(package_name)
-        
-        # Return the version of the package
-        return distribution.version
-    except pkg_resources.DistributionNotFound:
-        # Handle the case where the package is not installed
-        return None
-
-# Example usage for "auto-gptq"
-
-  def finalize(self,args):
-    self.tokenizer = None
-    self.model = None
+    def finalize(self, args):
+        pass
